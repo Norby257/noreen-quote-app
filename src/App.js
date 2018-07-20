5c2d1fc12e0a9836ca344dd680a80960c6fec85d
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-07-19 19:27:35 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Quotes from './Quotes.json';
+
+class App extends Component {
+
+  state = {
+    Quotes
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        {this.state.Quotes.map(quote => {
+          <li>{quote}</li>
+
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
